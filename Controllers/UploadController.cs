fc52a94ab734aa2d8350132dbd091061783160a6
Kumar Rishabh <kmr.rishabh6190@gmail.com>
2023-05-20 12:44:18 +0530

    Excluded dll files

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AnimalDrawing.Controllers
 {
+    //Controller
     public class UploadController : Controller
     {
         public IActionResult Index()
